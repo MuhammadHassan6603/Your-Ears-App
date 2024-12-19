b276d6d5a892c4e48191600ed15480bef11f49d8
HafizAbdulSaboor <abdulsaboor1956@gmail.com>
2024-12-19 15:15:13 +0500

    changes

--- lib/utils/images.dart
+++ lib/utils/images.dart
@@ -1,1 +1,5 @@
-class AppImages {}
+class AppImages {
+  //  png images
+  static const String onboardingImage = "assets/images/onboarding_2.png";
+  static const String onboardinglast = "assets/images/onboarding_3.png";
+}
