6a86ad36e2ac606f0e2403e42b25f3cec6d63180
HafizAbdulSaboor <abdulsaboor1956@gmail.com>
2024-12-19 14:21:26 +0500

    changes

---
+++ lib/routes/routes.dart
@@ -0,0 +1,9 @@
+part of 'routes_imports.dart';
+
+@AutoRouterConfig(replaceInRouteName: "Route")
+class AppRouter extends $AppRouter {
+  @override
+  List<AutoRoute> get routes => [
+    
+  ];
+}
---
+++ lib/routes/routes_imports.dart
@@ -0,0 +1,4 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:your_ears_app/routes/routes_imports.gr.dart';
+
+part 'routes.dart';
