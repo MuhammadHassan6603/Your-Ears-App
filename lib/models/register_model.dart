eb2104fa414d4134bc110e083743db1caac805cb
HafizAbdulSaboor <abdulsaboor1956@gmail.com>
2025-01-11 13:15:10 +0500

    changes

---
+++ lib/models/register_model.dart
@@ -0,0 +1,120 @@
+class registerModel {
+  String? message;
+  User? user;
+  Customer? customer;
+  String? token;
+
+  registerModel({this.message, this.user, this.customer, this.token});
+
+  registerModel.fromJson(Map<String, dynamic> json) {
+    message = json['message'];
+    user = json['user'] != null ? new User.fromJson(json['user']) : null;
+    customer = json['customer'] != null
+        ? new Customer.fromJson(json['customer'])
+        : null;
+    token = json['token'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['message'] = this.message;
+    if (this.user != null) {
+      data['user'] = this.user!.toJson();
+    }
+    if (this.customer != null) {
+      data['customer'] = this.customer!.toJson();
+    }
+    data['token'] = this.token;
+    return data;
+  }
+}
+
+class User {
+  String? name;
+  String? email;
+  String? phone;
+  String? updatedAt;
+  String? createdAt;
+  int? id;
+  Customer? customer;
+
+  User(
+      {this.name,
+      this.email,
+      this.phone,
+      this.updatedAt,
+      this.createdAt,
+      this.id,
+      this.customer});
+
+  User.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    email = json['email'];
+    phone = json['phone'];
+    updatedAt = json['updated_at'];
+    createdAt = json['created_at'];
+    id = json['id'];
+    customer = json['customer'] != null
+        ? new Customer.fromJson(json['customer'])
+        : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    data['email'] = this.email;
+    data['phone'] = this.phone;
+    data['updated_at'] = this.updatedAt;
+    data['created_at'] = this.createdAt;
+    data['id'] = this.id;
+    if (this.customer != null) {
+      data['customer'] = this.customer!.toJson();
+    }
+    return data;
+  }
+}
+
+class Customer {
+  int? id;
+  int? userId;
+  Null? profilePic;
+  Null? city;
+  Null? postalCode;
+  Null? dateOfBirth;
+  String? createdAt;
+  String? updatedAt;
+
+  Customer(
+      {this.id,
+      this.userId,
+      this.profilePic,
+      this.city,
+      this.postalCode,
+      this.dateOfBirth,
+      this.createdAt,
+      this.updatedAt});
+
+  Customer.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    userId = json['user_id'];
+    profilePic = json['profile_pic'];
+    city = json['city'];
+    postalCode = json['postal_code'];
+    dateOfBirth = json['date_of_birth'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['user_id'] = this.userId;
+    data['profile_pic'] = this.profilePic;
+    data['city'] = this.city;
+    data['postal_code'] = this.postalCode;
+    data['date_of_birth'] = this.dateOfBirth;
+    data['created_at'] = this.createdAt;
+    data['updated_at'] = this.updatedAt;
+    return data;
+  }
+}
